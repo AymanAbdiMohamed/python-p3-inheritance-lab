fc5d2676e8606c49ff62919a111a864f6446d552
ayman-abdi-mohamed <ayman.mohamed@student.moringaschool.com>
2025-11-23 10:27:52 +0300

    completed lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Teaching basics"]  # Must have at least one item
 
-    def teach(self):
-        pass
+    def teach(self, student=None, info=None):
+        """
+        If student and info are provided, teach the student.
+        If no info is provided, pick a random item from knowledge and return it.
+        """
+        if student and info:
+            student.learn(info)
+        else:
+            # Return a random knowledge item if called without arguments
+            return random.choice(self.knowledge)
